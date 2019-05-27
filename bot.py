b94d2bcb6a8d674113d3703d27b92654f6f9a4fa
StrafeStopper <strafestopper@gmail.com>
2019-05-27 02:45:06 -0600

    started over in python

---
+++ bot.py
@@ -0,0 +1,5 @@
+import praw
+
+reddit = praw.Reddit(user_agent='u/Strafebot-r by u/Stafe_Stopper',
+                     client_id='SDaOBSwKeqvYrQ', client_secret="BsE9BaBA_m0C__FsiinzKf6vuXs",
+                     username='Strafebot-r', password='360F%BepO5zK')
